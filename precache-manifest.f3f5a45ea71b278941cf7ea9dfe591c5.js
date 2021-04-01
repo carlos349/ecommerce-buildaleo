0978febdbd58e79a28bbdbdeb5f13f0ab332b5cc
carlos capacho <carlos.gomes349@gmail.com>
2021-04-01 14:17:26 -0300

    add change

---
+++ precache-manifest.f3f5a45ea71b278941cf7ea9dfe591c5.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ed809820d5e30545384",
+    "url": "/css/app.34b2bbac.css"
+  },
+  {
+    "revision": "1ed809820d5e30545384",
+    "url": "/js/app.8de48111.js"
+  },
+  {
+    "revision": "6b29b769e496d683eb5d",
+    "url": "/css/chunk-vendors.aa0adce1.css"
+  },
+  {
+    "revision": "6b29b769e496d683eb5d",
+    "url": "/js/chunk-vendors.7bb1f43a.js"
+  },
+  {
+    "revision": "f7d8c232fa0eef4d2853",
+    "url": "/css/demo.e0f36cbc.css"
+  },
+  {
+    "revision": "f7d8c232fa0eef4d2853",
+    "url": "/js/demo.07d6b13a.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "4291733509f3aa56eb797bcf5ca831d7",
+    "url": "/img/alpha-background.42917335.svg"
+  },
+  {
+    "revision": "04055494eb62645377845c78a1d109be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "59d7c2b119ab46fc7b5f853f04324c89",
+    "url": "/img/brand/green.png"
+  },
+  {
+    "revision": "eda72b64b46bbbfa767220ef5932ab3d",
+    "url": "/img/brand/imagotipo.png"
+  },
+  {
+    "revision": "f5dbc951c7df5a3809bca827726be919",
+    "url": "/img/brand/plus.png"
+  },
+  {
+    "revision": "e0ea3f1916671872498288dc2bd40f06",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "96ec3f26988e6e6e3a04745e4d969bad",
+    "url": "/img/brand/logo.png"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "/img/theme/angular.jpg"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "/img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "/img/theme/react.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "/img/theme/sketch.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "/img/theme/vue.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "/img/theme/profile-cover.jpg"
+  }
+];

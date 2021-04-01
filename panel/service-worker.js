97dbf0e83c714b8b58f54496fe3d89f86ddee228
carlos capacho <carlos.gomes349@gmail.com>
2021-04-01 18:06:45 -0300

    add changes

--- panel/service-worker.js
+++ panel/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82b68f19f9f5220985b5b0d782db5597.js"
+  "/precache-manifest.9fe37d0865d2c291944862e1dd111c51.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
